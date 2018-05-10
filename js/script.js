abdb00a81a093694d47debee09b981550d69b645
drbearcub <c.david092@gmail.com>
2018-05-10 12:49:52 -0700

    will fetch streetview with google api upon search

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    var streetStr = $('#street').val()
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('You wish to move to ' + address +'?');
+
+    var streetviewUrl = "https://maps.googleapis.com/maps/api/streetview?size=600x400&location=" +
+    address + '';
+    $body.append('<img class="bgimg", src="' + streetviewUrl + '">');
+    // load streetview
+
+    // YOUR CODE GOES HERE!
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
